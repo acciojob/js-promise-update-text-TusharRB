512097dabb2b53fa1879910f14efdd4da0df37b8
TusharRB <91899841+TusharRB@users.noreply.github.com>
2023-06-22 10:34:41 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 //your JS code here. If required.
+function updateText() {
+  return new Promise((resolve) => {
+    setTimeout(() => {
+      resolve("Hello, world!");
+    }, 1000);
+  });
+}
+
+// Retrieve the HTML element
+const outputElement = document.getElementById("output");
+
+// Call the updateText function and update the HTML element text after the promise resolves
+updateText().then((text) => {
+  outputElement.innerText = text;
+});
